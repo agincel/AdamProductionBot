b140efb6e709638bc64c3a1782ce2b4019a1110c
Adam Gincel <agincel@stevens.edu>
2020-04-28 00:13:35 -0400

    enemy display

--- src/dnd.js
+++ src/dnd.js
@@ -1344,10 +1344,11 @@ async function handle(text, platformObject, args, bots) {
             return await sendMessage(character.name + " restored " + heal + " SP. They are now at " + character.stats.sp + " SP.");
         }
     } else if (args[0] == "/enemies") {
-        let s = "Currently Spawned Enemies:\n\n";
+        let s = "Currently Spawned Enemies:\n\n```";
         for (let i = 0; i < group.enemies.length; i++) {
             s += i.toString() + ": " + group.enemies[i].name;
             let percentage = group.enemies[i].stats.currentHp / group.enemies[i].stats.hp;
+            s += " | ";
             if (percentage > .9) {
                 s += "😃";
             } else if (percentage > .7) {
@@ -1366,6 +1367,7 @@ async function handle(text, platformObject, args, bots) {
 
             s += "\n";
         }
+        s += "```";
 
         return await sendMessage(s);
     } else if (args[0] == "/pay") {
